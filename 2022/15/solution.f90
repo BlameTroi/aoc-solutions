8ab403b27ea1d850dd22c07c88e55e9ae78514bb
troi <blametroi@gmail.com>
2024-02-21 09:59:09 -0500

    2022 15, code needs work but it gets the job done.

--- 2022/15/solution.f90
+++ 2022/15/solution.f90
@@ -5,6 +5,16 @@
 ! given sensors that can identify the closest beacon, cast a net of sensors to
 ! map beacons. then determine where a beacon can not be in a specific row.
 
+! part two ups the volume (e, not n). there's a lot of unpruned experimental
+! stuff in here, but the right answers are found, and it wasn't as slow as i
+! feared to check all the points on a line between x=0 and x=4000000. leaving
+! the pruning for a future pass. there's stuff to learn from mistakes and
+! missteps.
+!
+! the unpruned code includes a lot of debug print type stuff that i want to
+! pull into some common modules at some point. commenting out most of the
+! actual writes for now.
+
 program solution
 
    use iso_fortran_env, only: int64
@@ -37,10 +47,21 @@ program solution
       integer(kind=int64) :: hi
    end type range_t
 
+   ! the slope intercept form of the line, y = m*x + b. for this problem we know
+   ! that slope is |1|, but we'll carry it around anyway. the interesting points
+   ! of the line lie in the region from 0 to 4,000,000 for both x and y.
+   ! according to the problem statement, we should find exactly one point that
+   ! is outside the range of any sensor. sadly, my best approach once i have
+   ! this information is to brute force scan.
+
+   integer(kind=int64), parameter  :: MINCOORD = 0
+   integer(kind=int64), parameter  :: MAXCOORD = 4000000 ! for small test data, 20
+
    type line_t
       integer(kind=int64) :: m ! y=m*x +b, in this problem m is + or - 1
-      integer(kind=int64) :: x
       integer(kind=int64) :: b
+      integer(kind=int64) :: x_at_y0              ! intercepts, but not the ones
+      integer(kind=int64) :: y_at_x4m             ! we usually look for
    end type line_t
 
    type(sensor_t) :: sensors(0:MAXDIM)
@@ -50,10 +71,19 @@ program solution
    integer(kind=int64) :: targety
    integer(kind=int64) :: mintyx
    integer(kind=int64) :: maxtyx
+
    type(range_t) :: ranges(0:MAXDIM)
    integer :: numranges
-   type(line_t) :: lines(0:MAXDIM * 4)
 
+   ! lines are in groups of four, with directions ...
+   integer, parameter :: wn = 0
+   integer, parameter :: se = 1
+   integer, parameter :: sw = 2
+   integer, parameter :: en = 3
+   type(line_t) :: lines(0:MAXDIM * 4)
+   integer :: numlines
+
+   integer(kind=int64) :: foundx, foundy
    ! initialize
 
    part_one = 0; part_two = 0
@@ -113,18 +143,18 @@ contains
 
       j = 0
 
-      write (*, *)
-      write (*, "('of ', i0, ' sensors, the following are of interest:')") numsensors
-      buf = sensor_range_prthdr()
-      write (*, *) trim(buf)
+      ! write (*, *)
+      ! write (*, "('of ', i0, ' sensors, the following are of interest:')") numsensors
+      ! buf = sensor_range_prthdr()
+      ! write (*, *) trim(buf)
       do i = 0, numsensors - 1
          associate (s => sensors(i))
             if (s % distance > s % tydistance) then
                adjx = s % distance - abs(s % y - targety)
                s % tyminx = s % x - adjx
                s % tymaxx = s % x + adjx
-               buf = sensor_range_prtstr(i)
-               write (*, *) trim(buf)
+               ! buf = sensor_range_prtstr(i)
+               ! write (*, *) trim(buf)
                ranges(j) % lo = s % tyminx; ranges(j) % hi = s % tymaxx
                j = j + 1
                mintyx = min(mintyx, s % tyminx)
@@ -135,31 +165,30 @@ contains
 
       numranges = j
 
-      call sort_ranges
-
       write (*, *) ""
-      write (*, *) "sorted ranges:"
-      do i = 0, numranges - 1
-         write (*, "(*(i14))") ranges(i) % lo, ranges(i) % hi
-      end do
+      write (*, *) "sorting ranges..."
+      call sort_ranges
+      ! do i = 0, numranges - 1
+      !    write (*, "(*(i14))") ranges(i) % lo, ranges(i) % hi
+      ! end do
 
       write (*, *)
       write (*, *) "collapsing ranges..."
       do while (collapse_ranges())
-         ! blip nothing yet
+         ! could require multiple passes
       end do
 
       write (*, *)
       write (*, *) "merging ranges..."
       do while (merge_ranges())
-         ! bloop also nothing yet
+         ! could require multiple passes
       end do
 
-      write (*, *)
-      write (*, *) "collapsed and merged ranges:"
-      do i = 0, numranges - 1
-         write (*, "(*(i14))") ranges(i) % lo, ranges(i) % hi
-      end do
+      ! write (*, *)
+      ! write (*, *) "collapsed and merged ranges:"
+      ! do i = 0, numranges - 1
+      !    write (*, "(*(i14))") ranges(i) % lo, ranges(i) % hi
+      ! end do
 
       ! this is a wrong answer for the big data
       write (*, *)
@@ -357,10 +386,10 @@ contains
       integer :: i
       character(len=:), allocatable :: t
 
-      write (*, *) "loading..."
+      !write (*, *) "loading..."
       i = 0
       do while (read_aoc_input(AOCIN, rec))
-         write (*, *) trim(rec)
+         !write (*, *) trim(rec)
          if (rec(1:9) /= "Sensor at") exit
          associate (s => sensors(i))
             t = sanatize(trim(rec))
@@ -428,6 +457,7 @@ contains
       character(len=*), parameter :: fmtcor = "('used grid coordinates range from ', a19, ' to ', a19)"
       character(len=200) :: buf
       integer :: i
+      return
       write (*, *)
       write (*, *) "our data"
       write (*, *)
@@ -444,50 +474,73 @@ contains
       write (*, *)
    end subroutine display_sensors
 
-! part two -- where is the beacon
+   ! part two -- where is the beacon
 
-! we are told that the beacon's x and y coordinates can range from 0 to 4000000.
-! determine the beacon's tuning frequency (x * 4000000 + y). there is only one
-! grid coordinate that can hold the beacon, what is its frequency?
-!
-! in the small test data, the location is x=14 y=11 for a frequency of 56000011.
-!
-! a brute force scan is feasible but will take forever. i'm trying to visualize
-! finding gaps but it isn't coming clear to me yet. one idea to play with is
-! sampling, just to keep me in the problem space and see what i might learn.
-!
-! as i think about it, unlike circles the manhattan distance gives us diamond
-! shapes. so there won't be the same sort of gap shapes that exist when working
-! with cartesian coordinates. gaps will be along parallel diagonal or cardinal
-! lines.
-!
-! i spent too much time trying to find a way to rotate the whole graph so i
-! could workd with rectangles instead of diamonds, but actually the diamond
-! shape is fine. since we know there is only one point that is not covered by
-! the sensors, it must be on a line that runs along one of the four edges of the
-! coverage diamond at range+1.
-!
-! the lines all have slopes of |1| so it's easy to figure out the intersections.
+   ! we are told that the beacon's x and y coordinates can range from 0 to
+   ! 4000000. determine the beacon's tuning frequency (x * 4000000 + y). there
+   ! is only one grid coordinate that can hold the beacon, what is its
+   ! frequency?
+   !
+   ! in the small test data, the location is x=14 y=11 for a frequency of
+   ! 56000011.
+   !
+   ! a brute force scan is feasible but will take forever. i'm trying to
+   ! visualize finding gaps but it isn't coming clear to me yet. one idea to
+   ! play with is sampling, just to keep me in the problem space and see what i
+   ! might learn.
+   !
+   ! as i think about it, unlike circles the manhattan distance gives us diamond
+   ! shapes. so there won't be the same sort of gap shapes that exist when
+   ! working with cartesian coordinates. gaps will be along parallel diagonal or
+   ! cardinal lines.
+   !
+   ! i spent too much time trying to find a way to rotate the whole graph so i
+   ! could workd with rectangles instead of diamonds, but actually the diamond
+   ! shape is fine. since we know there is only one point that is not covered by
+   ! the sensors, it must be on a line that runs along one of the four edges of
+   ! the coverage diamond at range+1.
+   !
+   ! the lines all have slopes of |1| so it's easy to figure out the
+   ! intersections.
+   !
+   ! the answer to this is the tuning frequency of the beacon which is x *
+   ! 4,000,000 + y. For the small test data is 56000011 for x=14, y = 11.
+   !
+   ! time to go brute force on this. find x and y intercept of each line, then
+   ! stepping along them is a matter of addition or subtraction.
+
+   ! y = mx + b
+   ! if y = 0, x = -b/m,
+   ! if x = 0, y = b
+   !
 
    function do_part_two() result(res)
       implicit none
       integer(kind=int64) :: res
       integer :: i, j
       integer(kind=int64) :: x, y
+      character(len=*), parameter :: fmtlin = "('    line ', a3, ' : ', a19, '-', a19)"
 
       x = 0; y = 0
+      foundx = 0; foundy = 0
       res = -1
 
+      write (*, *)
       ! determine endpoints of the open lines, all the lines have a slope of
-      ! |1|.
+      ! |1|. points on these lines are outside the range of the sensor they
+      ! surround. given problem statement, the can be only one point that holds
+      ! the out of range beacon. it must be on one of these lines.
       !
       ! n-e, w-s, w-n, s-e are the lines
-      ! n-e checks against all w-n and s-e
-      ! w-s checks against all w-n and w-e
 
       j = 0
       do i = 0, numsensors - 1
          associate (s => sensors(i))
+
+            ! the lines are defined by segments from the four diamon points that
+            ! are just out of range. these are nxy, exy, sxy, and wxy, giving
+            ! segments from w-n, s-e, s-w, and e-n.
+
             s % nx = s % x
             s % ny = s % y - s % distance - 1
 
@@ -498,23 +551,132 @@ contains
             s % sy = s % y + s % distance + 1
 
             s % wx = s % x - s % distance - 1
-            s % wy = y
+            s % wy = s % y
 
-            write (*, *) ""
-            write (*, "('sensor ', i0)") i
-            write (*, *) ""
-            write (*, *) "  negative slope:"
-            write (*, "('    line ', a3, ' : ', a19, '-', a19)") "n-e", point_str(s % nx, s % ny), point_str(s % ex, s % ey)
-            write (*, "('    line ', a3, ' : ', a19, '-', a19)") "w-s", point_str(s % wx, s % wy), point_str(s % sx, s % sy)
-            write (*, *) "  positive slope:"
-            write (*, "('    line ', a3, ' : ', a19, '-', a19)") "s-e", point_str(s % sx, s % sy), point_str(s % ex, s % ey)
-            write (*, "('    line ', a3, ' : ', a19, '-', a19)") "w-n", point_str(s % wx, s % wy), point_str(s % nx, s % ny)
+            ! find the line segments and add to lines. get_line populates the
+            ! full line_t and gives us endpoints for our brute force search
 
-            call intersection(x, y, s % nx, s % ny, s % ex, s % ey, s % wx, s % wy, s % nx, s % ny)
-            write (*, "('intersection should be ', a19, ' and is ', a19)") point_str(s % nx, s % ny), point_str(x, y)
+            lines(j + wn) = get_line(s % wx, s % wy, s % nx, s % ny)
+            lines(j + se) = get_line(s % sx, s % sy, s % ex, s % ey)
+            lines(j + sw) = get_line(s % sx, s % sy, s % wx, s % wy)
+            lines(j + en) = get_line(s % ex, s % ey, s % nx, s % ny)
+            j = j + 4
          end associate
       end do
+      numlines = j
+
+      ! now that we've built the lines, the slow process of checking them starts
+      ! point by point, line by line, against each sensor ...
+      lineloop: do i = 0, numlines - 1
+         associate (l => lines(i))
+            write (*, "('checking line ', i0)") i
+            !write (*, *) "checking line ", l % m, l % b, l % x_at_y0, l % y_at_x4m
+            xloop: do x = MINCOORD, MAXCOORD
+               y = l % m * x + l % b
+               ! if (mod(x, 1000000) == 0) write (*, *) point_str(x, y)
+               ! yes, this could be smarter
+               if (y < MINCOORD .or. y > MAXCOORD) cycle xloop
+               if (can_be_seen(x, y)) cycle xloop
+               foundx = x
+               foundy = y
+               res = x * MAXCOORD + y
+               write (*, *) "point can't be seen! ", point_str(x, y)
+               write (*, *) "answer: ", res
+               return
+            end do xloop
+         end associate
+      end do lineloop
    end function do_part_two
+
+   ! is a particular point in the valid range for this problem? 0->4000000
+
+   function possible_location(x, y) result(res)
+      implicit none
+      integer(kind=int64), intent(in) :: x, y
+      logical :: res
+      res = (x >= MINCOORD) .and. (x <= MAXCOORD) .and. (y >= MINCOORD) .and. (y <= MAXCOORD)
+   end function possible_location
+
+   ! is a particular point visible to any sensor?
+
+   function can_be_seen(x, y) result(res)
+      implicit none
+      integer(kind=int64), intent(in) :: x, y
+      logical :: res
+      integer(kind=int64) :: d
+      integer :: i
+
+      res = .false.
+      do i = 0, numsensors - 1
+         associate (s => sensors(i))
+            d = calculate_distance(x, y, s % x, s % y)
+            if (d <= s % distance) then
+               res = .true.
+               return
+            end if
+         end associate
+      end do
+   end function can_be_seen
+
+   ! given two points on a line (in this case, the diamond tips of our
+   ! 'just outside of range' boundary for a sensor) return a line structure
+   ! describing the line in terms of the slope intercept formula and provide
+   ! the coordates of the points where x = 4000000 or y = 0. i don't have
+   ! a faster solution yet than iterating over the line between those
+   ! points to see if it's in range of anything.
+
+   recursive function get_line(ax, ay, bx, by) result(res)
+      implicit none
+      integer(kind=int64), intent(in) :: ax, ay, bx, by
+      type(line_t) :: res
+
+      ! init
+      res % b = 0
+      res % m = 0
+      res % x_at_y0 = 0
+      res % y_at_x4m = 0
+
+      ! guard clause, ignore horiztonal and vertical lines for now.
+      ! the value of m=0 is used to flag the line as impossible
+      if (ax == bx .or. ay == by) then
+         return
+      end if
+
+      ! redispatch to get these all leaning the same way, left down
+      ! right up.
+      if (ax > bx) then
+         res = get_line(bx, by, ax, ay)
+         return
+      end if
+
+      call slope_intercept(res % m, res % b, ax, ay, bx, by)
+
+      res % x_at_y0 = find_x(res, 0_int64)
+      res % y_at_x4m = find_y(res, 4000000_int64)
+   end function get_line
+
+   function find_x(l, y) result(res)
+      implicit none
+      type(line_t), intent(in) :: l
+      integer(kind=int64), intent(in) :: y
+      integer(kind=int64) :: res
+      res = (y - l % b) / l % m
+   end function find_x
+
+   function find_y(l, x) result(res)
+      implicit none
+      type(line_t), intent(in) :: l
+      integer(kind=int64), intent(in) :: x
+      integer(kind=int64) :: res
+      res = l % m * x + l % b
+   end function find_y
+
+   function in_bounds(x, y) result(res)
+      implicit none
+      integer(kind=int64), intent(in) :: x, y
+      logical                         :: res
+      res = (x >= mincoord) .and. (x <= maxcoord) .and. (y >= mincoord) .and. (y <= maxcoord)
+   end function in_bounds
 
    ! given points on lines a-b and c-d, find their interesection. find the
    ! y=m*x+b equation (slope intercept form) for each line and then work from
