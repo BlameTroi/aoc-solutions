a22801bc5107e28404cb0845f2fbc3cd6c56ad6f
troi <blametroi@gmail.com>
2024-08-04 23:22:33 -0400

    2016 12 complete

---
+++ 2016/12/solution.h
@@ -0,0 +1,66 @@
+/* solution.h -- aoc 2016 12 -- troy brumley */
+
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+#define INPUT_LINE_MAX 4096
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+enum reg {
+   a = 0,
+   b,
+   c,
+   d
+};
+typedef enum reg reg;
+
+enum opcode {
+   inv = 0,
+   cpyr,
+   cpyv,
+   inc,
+   dec,
+   jnzr,
+   jnzv,
+   nop
+};
+typedef enum opcode opcode;
+
+typedef struct instruction instruction;
+struct instruction {
+   opcode opc;
+   int op1;
+   int op2;
+};
+
+typedef struct cpu cpu;
+struct cpu {
+   int ip;
+   int registers[4];
+   instruction core[32];
+};
+
+instruction
+assemble(char *);
+
+cpu *
+load(FILE *ifile);
+
+bool
+execute(cpu *);
+
+int
+part_one(
+   const char *                       /* a file name */
+);
+
+int
+part_two(
+   const char *                       /* a file name */
+);
+
+/* end solution.h */
+#endif /* SOLUTION_H */
---
+++ 2016/12/unittest.c
@@ -0,0 +1,162 @@
+/*  unittest.c -- shell for advent of code unit tests -- troy brumley */
+
+/*  because you should always make an effort to test first! */
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "minunit.h"
+#include "solution.h"
+#include "txbstr.h"
+
+/*
+ * minunit setup and teardown of infrastructure.
+ */
+
+bool got_args = false;
+int num_args = 0;
+char **the_args = NULL;
+char *filename = NULL;
+FILE *datafile = NULL;
+char *test_string = NULL;
+
+/*
+ * unittest (filename | -f filename) | -e string ...
+ */
+
+bool
+get_args(void) {
+   if (num_args < 2) {
+      got_args = true;
+   }
+   if (got_args) {
+      return true;
+   }
+
+   if (num_args == 2) {
+      filename = the_args[1];
+      got_args = true;
+      return got_args;
+   }
+
+   if (num_args > 2 && strcmp(the_args[1], "-f") == 0) {
+      filename = the_args[2];
+      got_args = true;
+      return got_args;
+   }
+
+   if (strcmp(the_args[1], "-i") != 0) {
+      fprintf(stderr, "error: illegal arguments to unittest\n");
+      exit(EXIT_FAILURE);
+   }
+
+   int len = num_args;
+   for (int i = 2; i < num_args; i++) {
+      len += strlen(the_args[i]);
+   }
+   test_string = malloc(len+1);
+   memset(test_string, 0, len+1);
+   for (int i = 2; i < num_args; i++) {
+      if (i != 2) {
+         strcat(test_string, " ");
+      }
+      strcat(test_string, the_args[i]);
+   }
+
+   got_args = true;
+   return got_args;
+}
+
+void
+test_setup(void) {
+   get_args();
+   if (filename) {
+      datafile = fopen(filename, "r");
+      if (!datafile) {
+         fprintf(stderr, "error: could not open test data file!\n");
+         exit(EXIT_FAILURE);
+      }
+   }
+}
+
+void
+test_teardown(void) {
+   if (datafile) {
+      fclose(datafile);
+   }
+}
+
+/*
+ * sample test shell.
+ */
+
+/*
+cpy 1 a
+cpy 1 b
+cpy 26 d
+jnz c 2
+jnz 1 5
+cpy 7 c
+inc d
+dec c
+jnz c -2
+cpy a c
+inc a
+dec b
+jnz b -2
+cpy c b
+dec d
+jnz d -6
+cpy 14 c
+cpy 14 d
+inc a
+dec d
+jnz d -2
+dec c
+jnz c -5
+*/
+/*
+  typedef enum opcode opcode;
+cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a
+  */
+
+
+MU_TEST(test_test) {
+   instruction c = assemble("cpy 41 a\n");
+   mu_should(c.opc == cpyv);
+   mu_should(c.op1 == 41);
+   mu_should(c.op2 == a);
+   mu_should(true);
+   mu_shouldnt(false);
+}
+
+/*
+ * here we defini the whole test suite.
+ */
+MU_TEST_SUITE(test_suite) {
+
+   MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+   /* run your tests here */
+   MU_RUN_TEST(test_test);
+}
+
+/*
+ * master control:
+ */
+
+int
+main(int argc, char *argv[]) {
+   num_args = argc;
+   the_args = argv;
+   MU_RUN_SUITE(test_suite);
+   MU_REPORT();
+   return MU_EXIT_CODE;
+}
